672b5b8e9aa28a1131bb754323c9fd3a1eef0ad7
donghyun <easygoinglife2@gmail.com>
2023-09-12 11:10:10 +0900

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const nodeExternals = require('webpack-node-externals')
+
+module.exports = {
+  mode: "production",
+  entry: "./app.js",
+  output: {
+    path: path.join(__dirname, "dist"),
+    publicPath: "/",
+    filename: "final.js",
+  },
+  target: "node",
+  externalsPresets: { node: true },
+  externals: [nodeExternals()]
+};
